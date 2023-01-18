a777516ec6f56f43b405dab196b571a910c58f0f
Vasily Prostyakov <frontprstvasya@gmail.com>
2023-01-18 12:00:57 +0300

    18.01

--- webpack.config.js
+++ webpack.config.js
@@ -43,8 +43,8 @@ module.exports = {
         }),
         new сopy({
             patterns: [
-                { from: path.join(__dirname, 'pages'), to: path.join(__dirname, "dist/pages") },
-                { from: path.join(__dirname, 'src/assets/img'), to: path.join(__dirname, "dist/src/assets/img") }
+                { from: path.join(__dirname, 'pages'), to: path.join(__dirname, "dist") },
+                { from: path.join(__dirname, 'src/assets/img'), to: path.join(__dirname, "dist/assets/img") }
 
             ]
         })
